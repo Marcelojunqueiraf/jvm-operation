ca68c5b23c3552e01bc20d7a160a127cd1716e03
Marcelojunqueiraf <marcelojunqueiraf@gmail.com>
2023-06-28 12:05:33 -0300

    arquivos q n tinha salvado

--- leitor/utf8.cpp
+++ leitor/utf8.cpp
@@ -10,7 +10,6 @@ char* Utf8_decoder(cp_info *cp_info_pointer){
     
     // pegar o tamanho
     u2 length = cp_info_pointer->constant_type_union.Utf8.length;
-    printf("length %d\n", length);
 
 
     // alocando espaço em memória para a string
@@ -18,8 +17,6 @@ char* Utf8_decoder(cp_info *cp_info_pointer){
     // alimentando a string com os caracteres?
     for(u2 i = 0; i < length; i++){
         // pegar o byte em questão e transfomar para char
-        printf("%p\n", cp_info_pointer->constant_type_union.Utf8.bytes);
-        printf("%d\n", cp_info_pointer->constant_type_union.Utf8.bytes[0]);
         u1 byte = cp_info_pointer->constant_type_union.Utf8.bytes[i];
         string[i] =  byte;
     };
