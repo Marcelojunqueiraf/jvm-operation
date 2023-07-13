6b1b40715b8ff63e0a59f36bb5c691a44ec930ff
Maycon Fabio <m.vinnicyus@gmail.com>
2023-07-13 19:07:53 -0300

    Increment the java examples

--- tests/print.java
+++ tests/print.java
@@ -1,5 +1,35 @@
 public class print{
   public static void main(String[] args){
-    System.out.println("Hello World!");
+    
+    
+    System.out.println("Hello World!"); // STRING
+    
+    System.out.println("BYTE");
+    System.out.println((byte) 127); // BYTE
+    
+    System.out.println("SHORT");
+    System.out.println((short) 32767); // SHORT
+    
+    System.out.println("INT");
+    System.out.println(2147483647); // INT
+    
+    System.out.println("LONG");
+    System.out.println(9223372036854775807l); // LONG
+    
+    char myChar = 'M';
+    System.out.println("CHAR");
+    System.out.println(myChar); // CHAR
+    
+    System.out.println("FLOAT");
+    System.out.println(3.14159265358979323846f); // FLOAT
+    
+    System.out.println("DOUBLE");
+    System.out.println(3.14159265358979323846); // DOUBLE
+    
+    System.out.println("BOOL");
+    System.out.println(true); // BOOL
+    
+    System.out.println("RETURNADDRESS");
+    System.out.println("qdiabéisso"); // RETURNADDRESS
   }
 }
