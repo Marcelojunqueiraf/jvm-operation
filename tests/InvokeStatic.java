8879d0d2265f550aec195216841a4bb01862252f
CeloPiano <marcelopianopatuscosantiago@gmail.com>
2023-07-13 21:15:24 -0300

    feat: invoke static .class and java files

---
+++ tests/InvokeStatic.java
@@ -0,0 +1,14 @@
+class animal {
+  
+  public static void grito() {
+    System.out.println("auuu");
+  }
+
+}
+
+public class InvokeStatic {
+  public static void main(String[] args) {
+    animal.grito();
+
+  }
+}
